07f0447f8bf80838d1416bedc8b6fa5150bd0f19
Smit Patel <sbhikadiya892@rku.ac.in>
2020-09-19 21:50:34 +0530

    Commit-14 - Change in MainActivity.java and activity_main.xml  > design change in activity_main.xml  > add some validation on edtNum1 and edtNum2  > add Paste functionality at beside the EditText  > add clear text functionality

--- app/src/main/java/com/spatel/calculator/MainActivity.java
+++ app/src/main/java/com/spatel/calculator/MainActivity.java
@@ -1,10 +1,13 @@
 package com.spatel.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.text.InputType;
 import android.text.method.DigitsKeyListener;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -13,17 +16,18 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.math.BigInteger;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btnAdd,btnSub,btnMul,btnDiv;
-    private EditText edtNum1,edtNum2;
-    private TextView txtResultBin,txtResultHex,txtResultOct,txtResultDes;
+    private Button btnAdd, btnSub, btnMul, btnDiv, btnPaste1, btnPaste2, btnClear;
+    private EditText edtNum1, edtNum2;
+    private TextView txtResultBin, txtResultHex, txtResultOct, txtResultDes;
     private Spinner spinner;
     private Converter converter;
-    private String sumD;
+    private String sumD = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnSub = findViewById(R.id.btnSub);
         btnMul = findViewById(R.id.btnMul);
         btnDiv = findViewById(R.id.btnDiv);
+        btnPaste1 = findViewById(R.id.btnPaste1);
+        btnPaste2 = findViewById(R.id.btnPaste2);
+        btnClear = findViewById(R.id.btnClear);
 
         txtResultBin = findViewById(R.id.txtResultBin);
         txtResultDes = findViewById(R.id.txtResultDes);
@@ -51,6 +58,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnDiv.setOnClickListener(this);
         btnMul.setOnClickListener(this);
         btnSub.setOnClickListener(this);
+        btnPaste1.setOnClickListener(this);
+        btnPaste2.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -61,32 +71,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-                setDigitAccordingToSpinner("DES");
+                //setDigitAccordingToSpinner("BIN");
             }
         });
     }
 
     private void setDigitAccordingToSpinner(String sp1) {
-        switch (sp1){
-            case "OCT":
+        switch (sp1) {
+            case "Octel":
                 edtNum1.setMaxLines(1);
                 edtNum2.setMaxLines(1);
                 edtNum1.setKeyListener(DigitsKeyListener.getInstance("0123456"));
                 edtNum2.setKeyListener(DigitsKeyListener.getInstance("0123456"));
                 break;
-            case "HEX":
+            case "HexaDesimal":
+                edtNum1.setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+                edtNum2.setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
                 edtNum1.setMaxLines(1);
                 edtNum2.setMaxLines(1);
-                edtNum1.setInputType(InputType.TYPE_CLASS_TEXT);
-                edtNum2.setInputType(InputType.TYPE_CLASS_TEXT);
                 edtNum1.setKeyListener(DigitsKeyListener.getInstance("0123456789ABCDEF"));
                 edtNum2.setKeyListener(DigitsKeyListener.getInstance("0123456789ABCDEF"));
                 break;
-            case "BIN":
+            case "Binary":
+                edtNum1.setMaxLines(3);
+                edtNum2.setMaxLines(3);
                 edtNum1.setKeyListener(DigitsKeyListener.getInstance("01"));
                 edtNum2.setKeyListener(DigitsKeyListener.getInstance("01"));
                 break;
-            case "DES":
+            case "Desimal":
                 edtNum1.setMaxLines(1);
                 edtNum2.setMaxLines(1);
                 edtNum1.setKeyListener(DigitsKeyListener.getInstance("0123456789"));
@@ -97,40 +109,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnPaste1:
+                pasteValue(edtNum1);
+                edtNum1.setSelection(edtNum1.getText().length());
+                break;
+            case R.id.btnPaste2:
+                pasteValue(edtNum2);
+                edtNum2.setSelection(edtNum2.getText().length());
+                break;
+            case R.id.btnClear:
+                edtNum1.setText("");
+                edtNum2.setText("");
+                break;
+        }
         String num1 = edtNum1.getText().toString().toUpperCase();
         String num2 = edtNum2.getText().toString().toUpperCase();
         String sp1 = spinner.getSelectedItem().toString();
-        if(!num1.equals("") && !num2.equals("")){
-            switch (view.getId()){
+        if (!num1.equals("") && !num2.equals("")) {
+            switch (view.getId()) {
                 case R.id.btnAdd:
-                    sumD = getResult(num1,num2,sp1,"Add");
+                    sumD = getResult(num1, num2, sp1, "Add");
                     break;
                 case R.id.btnSub:
-                    sumD = getResult(num1,num2,sp1,"Sub");
+                    sumD = getResult(num1, num2, sp1, "Sub");
                     break;
                 case R.id.btnDiv:
-                    sumD = getResult(num1,num2,sp1,"Div");
+                    sumD = getResult(num1, num2, sp1, "Div");
                     break;
                 case R.id.btnMul:
-                    sumD = getResult(num1,num2,sp1,"Mul");
+                    sumD = getResult(num1, num2, sp1, "Mul");
                     break;
             }
-            try{
-                txtResultBin.setText("Binary\n"+converter.desimalToBinary(sumD));
-                txtResultDes.setText("Desimal\n"+sumD.toUpperCase());
-                txtResultOct.setText("Octel\n"+converter.desimalToOctel(sumD));
-                txtResultHex.setText("HexaDesimal\n"+converter.desimalToHexa(sumD).toUpperCase());
-            }catch (NumberFormatException e){
-                Toast.makeText(this,"Too large number not allowed",Toast.LENGTH_SHORT).show();
+            try {
+                txtResultBin.setText("Binary\n" + converter.desimalToBinary(sumD));
+                txtResultDes.setText("Desimal\n" + sumD);
+                txtResultOct.setText("Octel\n" + converter.desimalToOctel(sumD));
+                txtResultHex.setText("HexaDesimal\n" + converter.desimalToHexa(sumD).toUpperCase());
+            } catch (NumberFormatException e) {
+                Toast.makeText(this, "Too large number is not allowed", Toast.LENGTH_SHORT).show();
                 return;
             }
-            setBackgroundOnText(getResources().getColor(R.color.colorAccent),getResources().getColor(R.color.colorWhite));
-        }
-        else{
-            Toast.makeText(this,"Enter a number first",Toast.LENGTH_SHORT).show();
-            setBackgroundOnText(getResources().getColor(R.color.colorDark),getResources().getColor(R.color.colorDark));
+            setBackgroundOnText(getResources().getColor(R.color.colorAccent), getResources().getColor(R.color.colorWhite));
+        } else {
+            Toast.makeText(this, "Empty field not accepted", Toast.LENGTH_SHORT).show();
+            setBackgroundOnText(getResources().getColor(R.color.colorDark), getResources().getColor(R.color.colorDark));
             return;
         }
+    }
+
+    private void pasteValue(EditText e) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = clipboard.getPrimaryClip();
+        ClipData.Item item = clip.getItemAt(0);
+        e.setText(item.getText().toString());
     }
 
     private void setBackgroundOnText(int color, int color1) {
@@ -145,44 +177,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private String getResult(String n1,String n2,String sp1,String str) {
+    private String getResult(String n1, String n2, String sp1, String str) {
         String result;
-        try{
-            switch (sp1){
-                case "OCT":
-                    n1 = converter.octelToDesimal(n1);
-                    n2 = converter.octelToDesimal(n2);
-                    break;
-                case "HEX":
-                    n1 = converter.hexaToDesimal(n1);
-                    n2 = converter.hexaToDesimal(n2);
-                    break;
-                case "BIN":
-                    n1 = converter.binaryToDesimal(n1);
-                    n2 = converter.binaryToDesimal(n2);
-                    break;
-            }
+        switch (sp1) {
+            case "Octel":
+                n1 = converter.octelToDesimal(n1);
+                n2 = converter.octelToDesimal(n2);
+                break;
+            case "HexaDesimal":
+                n1 = converter.hexaToDesimal(n1);
+                n2 = converter.hexaToDesimal(n2);
+                break;
+            case "Binary":
+                n1 = converter.binaryToDesimal(n1);
+                n2 = converter.binaryToDesimal(n2);
+                break;
         }
-        catch (Exception e){
-            return "";
+
+        if (n1.equals("") || n2.equals("")) {
+            if (n1.equals("")) n1 = "0";
+            else n2 = "0";
+            Toast.makeText(this,"Invalid Number",Toast.LENGTH_SHORT).show();
+            return "0";
         }
 
         BigInteger b1 = new BigInteger(n1);
-        if(str.equals("Add")){
+        if (str.equals("Add")) {
             result = b1.add(new BigInteger(n2)).toString();
-        }
-        else if(str.equals("Sub")){
+        } else if (str.equals("Sub")) {
             result = b1.subtract(new BigInteger(n2)).toString();
-        }
-        else if(str.equals("Div")){
-            try{
+        } else if (str.equals("Div")) {
+            try {
                 result = b1.divide(new BigInteger(n2)).toString();
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 return "";
             }
-        }
-        else{
+        } else {
             result = b1.multiply(new BigInteger(n2)).toString();
         }
         return result;
